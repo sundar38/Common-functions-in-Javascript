6a9f0557753f4efc51159c883ae758c7f7c7a93a
Penugonda <sundara.k.penugonda@accenture.com>
2023-01-07 17:35:47 +0530

    initial

--- script.js
+++ script.js
@@ -8,20 +8,54 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map(printingThroghMap); // Each element is stored in "printingThroghMap" variable
+ 
+  function printingThroghMap(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
+
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(printingThroghForEach); // Each element is stored in "printingThroghForEach" variable
+ 
+  function printingThroghForEach(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
 function addData() {
   //Write your code here
+  let newObj = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newObj);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  let filtered_arr = arr.filter(function (val) {
+    //callback function
+    if (val.profession !== "admin") {
+      //filtering other than admin criteria
+      return val;
+    }
+  });
+  console.log(filtered_arr);
 }
 
 function concatenateArray() {
-  //Write your code here
+  
+  let arr2 = [
+    { id: 5, name: "sundar", age: "25", profession: "developer" },
+    { id: 6, name: "surya", age: "23", profession: "developer" },
+    { id: 7, name: "vsuma", age: "21", profession: "Manager" },
+  ];
+ 
+  let concatenatedArray = arr.concat(arr2);
+  console.log(concatenatedArray);
 }
